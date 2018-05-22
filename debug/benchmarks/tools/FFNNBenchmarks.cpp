b91c8dff1fe40c2126aed020778781c21b884db1
Jan Kessler <jakessle@t-online.de>
2018-05-22 20:42:16 +0200

    Benchmarks: Mini leak fix

--- debug/benchmarks/tools/FFNNBenchmarks.cpp
+++ debug/benchmarks/tools/FFNNBenchmarks.cpp
@@ -5,25 +5,29 @@
 
 double benchmark_FFPropagate(FeedForwardNeuralNetwork * const ffnn, const double * const * const xdata, const int neval) {
     Timer * const timer = new Timer();
+    double time;
 
     timer->reset();
     for (int i=0; i<neval; ++i) {
         ffnn->setInput(xdata[i]);
         ffnn->FFPropagate();
     }
-    return timer->elapsed();
+    time = timer->elapsed();
+
+    delete timer;
+    return time;
 }
 
 double benchmark_actf_derivs(ActivationFunctionInterface * actf, const double * const xdata, const int neval, const bool flag_d1 = true, const bool flag_d2 = true, const bool flag_d3 = true, const bool flag_fad = true) {
     Timer * const timer = new Timer();
-    double v, v1d, v2d, v3d;
+    double time, v, v1d, v2d, v3d;
 
     if (flag_fad) {
         timer->reset();
         for (int i=0; i<neval; ++i) {
             actf->fad(xdata[i], v, v1d, v2d, v3d, flag_d1, flag_d2, flag_d3);
         }
-        return timer->elapsed();
+        time = timer->elapsed();
     }
     else {
         timer->reset();
@@ -33,6 +37,9 @@ double benchmark_actf_derivs(ActivationFunctionInterface * actf, const double * 
             v2d = flag_d2 ? actf->f2d(xdata[i]) : 0.0;
             v3d = flag_d3 ? actf->f3d(xdata[i]) : 0.0;
         }
-        return timer->elapsed();
+        time = timer->elapsed();
     }
+
+    delete timer;
+    return time;
 }
