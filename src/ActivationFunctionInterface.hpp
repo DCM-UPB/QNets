74f90eb194153eb014fb62dc8e2655e333674016
Jan Kessler <jakessle@t-online.de>
2018-05-22 22:30:19 +0200

    Due to benchmark results, a direct instead of virtual fad implementation was added for all remaining actfs.

--- src/ActivationFunctionInterface.hpp
+++ src/ActivationFunctionInterface.hpp
@@ -26,10 +26,12 @@ public:
     // third derivative of the activation function
     virtual double f3d(const double &) = 0;
 
-    // function to calculate all needed derivatives together, which potentially allows for speedup
+    // function to calculate function value and all needed derivatives together, allowing for speedup over individual calls
     virtual void fad(const double &in, double &v, double &v1d, double &v2d, double &v3d, const bool flag_d1 = false, const bool flag_d2 = false, const bool flag_d3 = false)
     {
-        // generic implementation
+        // Generic virtual implementation, overwriting it will generally yield faster function calls!
+        // If possible, the fad function should at least break even in speed if at least 1 derivative is required and be faster for 2 or more.
+
         v = f(in);
         v1d = flag_d1 ? this->f1d(in) : 0.0;
         v2d = flag_d2 ? this->f2d(in) : 0.0;
--- src/ReLUActivationFunction.cpp
+++ src/ReLUActivationFunction.cpp
@@ -25,3 +25,17 @@ double ReLUActivationFunction::f3d(const double &in)
 {
     return 0.0;
 }
+
+void ReLUActivationFunction::fad(const double &in, double &v, double &v1d, double &v2d, double &v3d, const bool flag_d1, const bool flag_d2, const bool flag_d3)
+{
+    if (in>0.0) {
+        v = in;
+        v1d = flag_d1 ? 1.0 : 0.0;
+    }
+    else {
+        v = _alpha*in;
+        v1d = flag_d1 ? _alpha : 0.0;
+    }
+    v2d = 0.0;
+    v3d = 0.0;
+}
