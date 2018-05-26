dbc52d26a1fc6b3b4f9e3774e8751a0729d6c73a
Jan Kessler <jakessle@t-online.de>
2018-05-26 22:45:32 +0200

    NNUnit: Cleaner derivative flag passing

--- src/NNUnit.cpp
+++ src/NNUnit.cpp
@@ -11,7 +11,10 @@ void NNUnit::computeValues(){
         // shared useful values
         double a1d, a2d, a3d;
 
-        _actf->fad(_pv, _v, a1d, a2d, a3d, _v1d || _v2d || _v1vd || _v1d1vd, _v2d || _v1vd, _v2d1vd);
+        const bool flag_d1 = _v1d || _v2d || _v1vd || _v1d1vd;
+        const bool flag_d2 = _v2d || _v1vd;
+        const bool flag_d3 = _v2d1vd;
+        _actf->fad(_pv, _v, a1d, a2d, a3d, flag_d1, flag_d2, flag_d3);
 
         if (_v1d || _v2d){
             for (int i=0; i<_nx0; ++i) _first_der[i] = _feeder->getFirstDerivativeFeed(i);
