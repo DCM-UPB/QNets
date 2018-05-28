4982071fe14cd85bb2a56c9d49470bd109cf7641
Jan Kessler <jakessle@t-online.de>
2018-05-29 00:01:18 +0200

    Benchmarks: plot script iteration

--- benchmarks/bench_actfs_derivs/plot.py
+++ benchmarks/bench_actfs_derivs/plot.py
@@ -1,6 +1,6 @@
 from pylab import *
 
-class benchmark_container:
+class benchmark_actf_derivs:
 
     def __init__(self, filename, label):
         self.label = label
@@ -12,52 +12,61 @@ class benchmark_container:
 
                 lsplit = line.split()
 
-                if len(lsplit) < 2:
+                if len(lsplit) < 5:
                     continue
 
                 if lsplit[0] == 'ACTF':
                     if not bnew:
                         self.data[actf_name] = actf_data # store previous actf's data
 
-                    actf_name = lsplit[7]
+                    actf_name = lsplit[10]
                     actf_data = {}
                     bnew = False
                     continue
 
-                if lsplit[1] == 'function':
-                    new_mode = lsplit[0]
-                    if new_mode == 'fad':
-                        actf_data[mode] = mode_data # store individual mode data
-                    mode = new_mode
-                    mode_data = {}
-                    continue
+                if len(lsplit) > 5:
+                    if lsplit[5] == 'function':
+                        new_mode = lsplit[4]
+                        if new_mode == 'fad':
+                            actf_data[mode] = mode_data # store individual mode data
+                        mode = new_mode
+                        mode_data = {}
+                        continue
 
                 if lsplit[0][0:2] == 'f:' or lsplit[0][0:2] == 'f+':
-                    mode_data[lsplit[0][:-1]] = float(lsplit[1])
+                    mode_data[lsplit[0][:-1]] = (float(lsplit[1]), float(lsplit[3]))
 
                 if lsplit[0] == 'f+d1+d2+d3:' and mode=='fad':
                     actf_data[mode] = mode_data # store fad mode data
 
         self.data[actf_name] = actf_data # store last actf's data
 
+def plot_compare_actfs(benchmark_list, **kwargs):
 
-mybenchmark = benchmark_container('benchmark_new.out', 'new')
-print(mybenchmark.label)
-print(mybenchmark.data)
-print(mybenchmark.data['lgs']['fad'].keys(), mybenchmark.data['lgs']['fad'].values())
+    nbm = len(benchmark_list)
+
+    fig = figure()
+    itp = 0
+    for benchmark in benchmark_list:
+        for itm, mode in enumerate(['individual', 'fad']):
+            itp+=1
+            ax = fig.add_subplot(nbm, 2, itp)
+            ax.set_title(benchmark.label + ' version, ' + mode + ' function calls')
+            ax.set_ylabel('Time per evaluation [ns]')
+            for actf in benchmark.data.keys():
+                ax.errorbar(benchmark.data[actf][mode].keys(), [v[0] for v in benchmark.data[actf][mode].values()], xerr=None, yerr=[v[1] for v in benchmark.data[actf][mode].values()], **kwargs)
+            ax.legend(benchmark.data.keys())
+    fig.tight_layout()
+    return fig
 
 
-actfs = ['lgs', 'gss', 'tans', 'id_', 'sin', 'relu', 'selu']
+benchmark_new = benchmark_actf_derivs('benchmark_new.out', 'new')
+try:
+    benchmark_old = benchmark_actf_derivs('benchmark_old.out', 'old')
+    benchmark_list = [benchmark_new, benchmark_old]
+except:
+    benchmark_list = [benchmark_new]
 
-figure(1)
-for actf in actfs:
-    plot(mybenchmark.data[actf]['individual'].keys(), mybenchmark.data[actf]['individual'].values(), 'o--')
-legend(actfs)
+fig1 = plot_compare_actfs(benchmark_list, fmt='o--')
 
-figure(2)
-for actf in actfs:
-    plot(mybenchmark.data[actf]['fad'].keys(), mybenchmark.data[actf]['fad'].values(), 'o--')
-legend(actfs)
-
-tight_layout()
 show()
