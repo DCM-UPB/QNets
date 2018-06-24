5624d1eb52c09527c7c30a13134fe8e603c0be2c
Jan Kessler <jakessle@t-online.de>
2018-06-24 23:03:31 +0200

    Added debug/run_all.sh script to compile/test via /debug/run.sh and then run all unittests via /debug/unittest/run.sh

---
+++ debug/run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Runs the debug compile test and then all unittests
+# NOTE: After using this script it is necessary to run again the regular 
+# build.sh script for generating again the library with the optimization flags
+
+./run.sh && cd unittest && ./run.sh
