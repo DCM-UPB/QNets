d7cef353899147e8e88a719368b475d02939467b
Jan Kessler <jakessle@t-online.de>
2018-07-21 20:26:52 +0200

    Added Smooth Rectified Linear Unit Activation Function ln(1+exp(x)), #27

--- src/actf/ActivationFunctionManager.cpp
+++ src/actf/ActivationFunctionManager.cpp
@@ -7,18 +7,20 @@ namespace std_actf{
     IdentityActivationFunction id_actf = IdentityActivationFunction();
     LogisticActivationFunction lgs_actf = LogisticActivationFunction();
     GaussianActivationFunction gss_actf = GaussianActivationFunction();
+    TanSigmoidActivationFunction tans_actf = TanSigmoidActivationFunction();
     ReLUActivationFunction relu_actf = ReLUActivationFunction();
     SELUActivationFunction selu_actf = SELUActivationFunction();
-    TanSigmoidActivationFunction tans_actf = TanSigmoidActivationFunction();
+    SRLUActivationFunction srlu_actf = SRLUActivationFunction();
     SineActivationFunction sin_actf = SineActivationFunction();
 
     std::vector<ActivationFunctionInterface *> supported_actf = {
         &id_actf,
         &lgs_actf,
         &gss_actf,
+        &tans_actf,
         &relu_actf,
         &selu_actf,
-        &tans_actf,
+        &srlu_actf,
         &sin_actf
     };
 
