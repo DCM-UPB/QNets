6e51a9ebfc583046eef55956c33cf48e9b7686f3
Jan Kessler <jakessle@t-online.de>
2019-02-21 21:12:46 +0100

    Made build.sh more portable and fixed OpenMP build

--- config_template.sh
+++ config_template.sh
@@ -9,7 +9,7 @@ CXX_FLAGS="-O3 -flto -Wall -Wno-unused-function"
 # add coverage flags
 USE_COVERAGE=0
 
-# use MPI for integration
+# use OpenMP for parallel propagation
 USE_OPENMP=0
 
 # GNU Scientific Library
